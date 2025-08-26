195e97edf084e26bf5ff8b16b0c4a40949523d3e
IUEmirhanDemir <127096710+IUEmirhanDemir@users.noreply.github.com>
2025-08-27 01:44:35 +0200

    Remove App component implementation

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
-import { Sidebar } from './sidebar';
-
-@Component({
-  selector: 'app-root',
-  imports: [Sidebar],
-  templateUrl: './app.html',
-  standalone: true,
-  styleUrls: ['./app.css']
-})
-export class App {
-}
+ssss
