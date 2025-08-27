c69c1ba02de5f63c27285d9b6f64bbe8b563a020
IUEmirhanDemir <127096710+IUEmirhanDemir@users.noreply.github.com>
2025-08-27 14:44:12 +0200

    Update app.config.ts

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -3,10 +3,4 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
 };
