f30fa3483791e783d54864b93447dd3ed71bf1af
emir <demir.emirhan@gmx.de>
2025-09-26 22:16:32 +0200

    First Steps

---
+++ server/src/main/java/com/emirhan/demir/noteai/NoteAIApplication.java
@@ -0,0 +1,13 @@
+package com.emirhan.demir.noteai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteAIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteAIApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/emirhan/demir/noteai/NoteAIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirhan.demir.noteai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
